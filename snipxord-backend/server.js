0e2249588bee3b519556024a3f6aaa217b403a4b
mickel1122 <mohit9015157818mohit@gmail.com>
2025-04-19 15:59:04 +0530

    Add snipxord-backend/server.js 1745058540601

---
+++ snipxord-backend/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: '*',
+  }
+});
+
+app.use(cors());
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Snipxord backend server is running');
+});
+
+// Socket.io connection
+io.on('connection', (socket) => {
+  console.log('New client connected:', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected:', socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
